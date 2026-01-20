9d3804604dbfb36040a5283cc1f425a1902a6bbb
Dimas Fatahila <alexeidimakhov@gmail.com>
2026-01-20 21:30:52 +0700

    Create index.js

---
+++ api/index.js
@@ -0,0 +1,22 @@
+[
+  {
+    "movie_id": 2,
+    "title": "Ariel",
+    "poster": "https://image.tmdb.org/t/p/w500/ojDg0PGvs6R9xY...jpg",
+    "release_date": "1988-10-21",
+    "runtime": 73,
+    "title_seo": "Ariel | Movie Info",
+    "overview_seo": "A Finnish man goes to the city to find a job...",
+    "slug": "ariel"
+  },
+  {
+    "movie_id": 11,
+    "title": "Star Wars",
+    "poster": "https://image.tmdb.org/t/p/w500/6FfCtAuVAW8XJj...jpg",
+    "release_date": "1977-05-25",
+    "runtime": 121,
+    "title_seo": "Star Wars | Movie Info",
+    "overview_seo": "Princess Leia is captured and held hostage...",
+    "slug": "star-wars"
+  }
+]
