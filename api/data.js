44fd04c39fe2841e96ba231630988d5b910c2d74
Dimas Fatahila <alexeidimakhov@gmail.com>
2026-01-22 13:28:30 +0700

    1

--- api/data.js
+++ api/data.js
@@ -16,11 +16,13 @@ module.exports = async (req, res) => {
 
     let xml = `<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">`;
     
-    // Tambah homepage hanya di part 1
     if (p === '1') xml += `<url><loc>${baseUrl}/</loc><priority>1.0</priority></url>`;
 
     selected.forEach(g => {
-      if (g.slug) xml += `<url><loc>${baseUrl}/${g.slug}</loc></url>`;
+      // PERBAIKAN: Disinkronkan dengan single.html?id= agar link bisa dibuka Google
+      if (g.slug) {
+        xml += `<url><loc>${baseUrl}/single.html?id=${g.slug}</loc></url>`;
+      }
     });
 
     xml += `</urlset>`;
