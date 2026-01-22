06083818fbfe75a398a835339bea226ce0465802
Dimas Fatahila <alexeidimakhov@gmail.com>
2026-01-22 14:53:34 +0700

    Update sitemap.js

--- api/sitemap.js
+++ api/sitemap.js
@@ -1,11 +1,24 @@
 module.exports = async (req, res) => {
   const baseUrl = 'https://gusku.site';
+  // Mengambil tanggal hari ini secara otomatis
+  const today = new Date().toISOString().split('T')[0]; 
+
   const xml = `<?xml version="1.0" encoding="UTF-8"?>
 <sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-  <sitemap><loc>${baseUrl}/s1.xml</loc></sitemap>
-  <sitemap><loc>${baseUrl}/s2.xml</loc></sitemap>
-  <sitemap><loc>${baseUrl}/s3.xml</loc></sitemap>
+  <sitemap>
+    <loc>${baseUrl}/s1.xml</loc>
+    <lastmod>${today}</lastmod>
+  </sitemap>
+  <sitemap>
+    <loc>${baseUrl}/s2.xml</loc>
+    <lastmod>${today}</lastmod>
+  </sitemap>
+  <sitemap>
+    <loc>${baseUrl}/s3.xml</loc>
+    <lastmod>${today}</lastmod>
+  </sitemap>
 </sitemapindex>`;
+
   res.setHeader('Content-Type', 'text/xml');
   res.send(xml);
 };
