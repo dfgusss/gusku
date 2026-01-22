1ec547b47c1f72036d245ba440c2f699a4617144
Dimas Fatahila <alexeidimakhov@gmail.com>
2026-01-22 18:36:56 +0700

    1

--- api/index.js
+++ api/index.js
@@ -1,16 +1,20 @@
+
 module.exports = async (req, res) => {
   const listUrl = 'https://raw.githubusercontent.com/dfgusss/gusku/main/list_games.json';
+
   try {
     const response = await fetch(listUrl);
     if (!response.ok) throw new Error('File list_games.json tidak ditemukan');
     
     const allGames = await response.json();
+
+    // Mengacak data dan mengambil tepat 10 game sesuai grid 5x2 di index.html
     const shuffled = allGames.sort(() => 0.5 - Math.random());
     const selectedGames = shuffled.slice(0, 10);
 
+    // Mengirimkan data dalam format JSON, bukan HTML
     res.setHeader('Content-Type', 'application/json');
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Cache-Control', 'public, max-age=300, stale-while-revalidate=59'); // caching
+    res.setHeader('Access-Control-Allow-Origin', '*'); // Izin agar bisa dibaca oleh index.html
     res.status(200).json(selectedGames);
 
   } catch (err) {
